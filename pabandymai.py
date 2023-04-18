2b0dc94cdc4c3ca356256531f7ac9743e1b8a256
RenaldasZ <spikeris2@gmail.com>
2023-04-18 20:36:18 +0300

    update

--- pabandymai.py
+++ pabandymai.py
@@ -1,30 +1,39 @@
-saldytuvas = {}
+# Užduotis - funkijų išnaudojimas basic projekte
+saldytuvas = {'spageciai': [1, "kg"], 'pomidoru padazas': [1, "kg"], 'suris': [1, "kg"]}
+receptai = {
+    "Spageciai su suriu": {'spageciai': 1, 'pomidoru padazas': 1, 'suris': 1}, \
+    "sumustinis 1": {"batonas": 1, "sviestas": 1, "suris": 1}, \
+    'blynai': {'kiausinis': 2,'pienas' : 0.5 , 'miltai': 1}
+}
 
 def remove_product():
-    name = input('Įveskite produkto pavadinimą: ')
+    name = input('iveskite produkta: ')
 
     if name in saldytuvas.keys():
         if saldytuvas[name][0] > 1:
             saldytuvas[name][0] -= 1
+
         else:
             saldytuvas.pop(name)
+        return
 
 def add_product(product_name):
     quantity = float(input("Įveskite kiekį: "))
-    unit = input("vienetai: ")
-
+    unit = input("unit: ")
     if unit == "kg":
-        quantity *= 1000  # convert kg to g
+        quantity *= 1  # convert kg to g
 
     if product_name in saldytuvas: 
-        saldytuvas[product_name] += [quantity, unit]
+        saldytuvas[product_name][0] += quantity
+
     else:
         saldytuvas[product_name] = [quantity, unit]
-    
+
     print("Produktas sėkmingai pridėtas į šaldytuvą!\n")
     print(saldytuvas)
 
 def view_product():
+
     for key, value in saldytuvas.items():
         print(key, value)
 
@@ -34,31 +43,47 @@ def total_mass():
     total_vienetas = 0
 
     for value in saldytuvas.values():
+
         if value[1] == 'kg':
-            total_kg += value[0] / 1000  # convert g to kg
+            total_kg += value[0]
+
         elif value[1] == 'l':
             total_l += value[0]
+
         else:
             total_vienetas += value[0]
 
-    print("Bendra produktų masė: {:.2f} kg ir {:.2f} l, {} vienetai".format(total_kg, total_l, total_vienetas))
+    print("Bendra produktų masė: {:.2f} kg ir {:.2f} l, {} vienetai".format(total_kg/1, total_l, total_vienetas))
+
+def ar_iseina():
+    for key in receptai.keys():
+        print(key)
+
+    x = input("ka norite gaminti?: ")
+    if x in receptai.keys():
+        for key, val in receptai[x].items():
+            if key in saldytuvas and val <= saldytuvas[key][0]:
+                print(f'sito produkto {key} uztenka')
+            else:
+                print(f'sito produkto {key} neuztenka')
 
 while True:
     print("""
-    1 - Pridėti produktą
-    2 - Išimti produktą
-    3 - Apžiūrėti produktus šaldytuve
-
-    0 - Išeiti
+    1- prideti produkta
+    2- isimti produkta
+    3- apziureti produktus saldytuve
+    4- ar iseina
+    0- iseiti
+    
     """)
-
-    choice = int(input("Pasirinkite funkciją: "))
+    choice = int(input("pasirinkite funkcija: "))
 
     if choice == 1:
-        name = input("Produkto pavadinimas: ")
+        name = input("produkto pavadinimas: ")
         add_product(name)
 
     elif choice == 2:
+        
         print(saldytuvas)
         remove_product()
         print(saldytuvas)
@@ -67,9 +92,24 @@ while True:
         view_product()
         total_mass()
 
+    elif choice == 4:
+        ar_iseina()
+
     elif choice == 0:
         print("Viso gero")
         break
 
     else:
-        print('Neteisingas pasirinkimas.')
+        print('')
+        
+# galiu paaiškinti, ką reiškia kai kurių kodinio pavyzdžio eilučių kodas:
+
+# Eilutėje 1 yra sukurtas žodynas (dictionary) "saldytuvas", kuriame saugoma informacija apie produktų kiekius ir vienetus.
+# Eilutėje 2 yra sukurtas žodynas "receptai", kuriame saugoma informacija apie receptus ir reikalingus produktų kiekius.
+# Eilutėje 5 pradedama "remove_product()" funkcija, skirta ištrinti produktą iš saldytuvo.
+# Eilutėje 15 pradedama "add_product()" funkcija, skirta pridėti produktą į saldytuvą.
+# Eilutėje 23 pradedama "view_product()" funkcija, skirta peržiūrėti produktus, esančius saldytuve.
+# Eilutėje 34 pradedama "total_mass()" funkcija, skirta apskaičiuoti bendrą produktų masę, esančią saldytuve.
+# Eilutėje 43 pradedama "ar_iseina()" funkcija, skirta patikrinti, ar galima pagaminti pasirinktą receptą iš produktų, esančių saldytuve.
+# Eilutėje 50-61 yra meniu, kuriame vartotojas gali pasirinkti, kokią funkciją vykdyti.
+# Eilutėje 63 programa baigiasi.
